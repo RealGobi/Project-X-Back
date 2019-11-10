a40f6bc4829dc303950dc20b57af26ab5aad5992
Jimmy Svensson <jimmy888swe@gmail.com>
2019-11-10 22:38:47 +0100

    added cors handling and req.body

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
+//Get
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'order was fetched'
     });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'order was created'
-    });
-});
-
+//Get by id
 router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'order details',
@@ -20,6 +15,19 @@ router.get('/:orderId', (req, res, next) => {
     });
 });
 
+//Post
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'order was created',
+        order: order
+    });
+});
+
+//Delete by id
 router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'order deleted',
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 // Get
-
 router.get('/', (req, res, next) => {
     res.status(200).json({
         messege: 'GET req to /products'
@@ -10,15 +9,20 @@ router.get('/', (req, res, next) => {
 });
 
 // Post
-
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
     res.status(201).json({
-        messege:  'POST req to /products'
+        messege:  'POST req to /products',
+        createdProduct: product
     })
 });
 
+//Get by id
 router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
+    const id = req.params.productId; 
     if (id === 'special') {
         res.status(200).json({
             messege: 'Special ID for singel view',
@@ -31,12 +35,14 @@ router.get('/:productId', (req, res, next) => {
     }
 });
 
+// Patch
 router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
         messege: 'Updated product'
     });
 });
 
+//Delete by id
 router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
         messege: 'Deleted product'
