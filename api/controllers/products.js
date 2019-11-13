f5faf84fbc28b7f16383dd7f42f18a77aaee1377
Jimmy Svensson <jimmy888swe@gmail.com>
2019-11-13 22:39:29 +0100

    added controller to better structur

---
+++ api/controllers/products.js
@@ -0,0 +1,127 @@
+const mongoose = require('mongoose');
+const Product = require('../models/products');
+
+exports.getAllProducts = (req, res, next) => {
+    Product.find()
+    .select('name price _id')
+    .exec()
+    .then(docs => {
+        const respones = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + doc._id
+                    }
+                };
+            })
+        };
+        res.status(200).json(respones);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+};
+
+exports.postProducts = (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            messege:  'Created successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
+}
+
+exports.updateProducts = (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+// look only att feild that has change ex. to update[ { "propName": "name", "value": "jimmy" },{ "propName": "price", "value": "999" }]
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({_id: id}, { $set: updateOps })
+    .exec()
+    .then(
+        res.status(200).json({
+            message: 'Product Updated',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/products/' + id
+            }
+        })
+    )
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
+}
+
+exports.getSingleProducts = (req, res, next) => {
+    // get product id from url
+        const id = req.params.productId; 
+        Product.findById(id)
+        .select('name price _id')
+        .exec()
+        .then(doc => {
+            console.log('From DB', doc);
+            if(doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({
+                    message: 'No valid Id in database.'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err})
+        });
+    }
+
+exports.deleteProducts =  (req, res, next) => {
+    const id = req.params.productId
+    Product.deleteOne({_id: id})
+    .exec()
+    .then(
+        res.status(200).json({
+            message: "Deleted Successfully"
+        })
+    )
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
+}
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,136 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const Product = require('../models/products');
+const ProductController = require('../controllers/products');
+
+// check auth
+const checkAuth = require('../middleware/checkAuth');
+
+
+//handeling routes
 
 // Get
-router.get('/', (req, res, next) => {
-    Product.find()
-    .select('name price _id')
-    .exec()
-    .then(docs => {
-        const respones = {
-            count: docs.length,
-            products: docs.map(doc => {
-                return {
-                    name: doc.name,
-                    price: doc.price,
-                    _id: doc._id,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/products/' + doc._id
-                    }
-                };
-            })
-        };
-        res.status(200).json(respones);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
+router.get('/', ProductController.getAllProducts);
 
 // Post
-router.post('/', (req, res, next) => {
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price
-    });
-    product
-    .save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            messege:  'Created successfully',
-            createdProduct: {
-                name: result.name,
-                price: result.price,
-                _id: result._id,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:3000/products/' + result._id
-                }
-            }
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
-});
+router.post('/', checkAuth, ProductController.postProducts);
 
 //Get by id
-router.get('/:productId', (req, res, next) => {
-// get product id from url
-    const id = req.params.productId; 
-    Product.findById(id)
-    .select('name price _id')
-    .exec()
-    .then(doc => {
-        console.log('From DB', doc);
-        if(doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({
-                message: 'No valid Id in database.'
-            });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error: err})
-    });
-});
+router.get('/:productId', ProductController.getSingleProducts);
 
 // Patch $set-mongoose 
-router.patch('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    const updateOps = {};
-// look only att feild that has change ex. to update { "propName": "name", "value": "jimmy" },{ "propName": "price", "value": "999" }
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
-    }
-    Product.updateOne({_id: id}, { $set: updateOps })
-    .exec()
-    .then(
-        res.status(200).json({
-            message: 'Product Updated',
-            request: {
-                type: 'GET',
-                url: 'http://localhost:3000/products/' + id
-            }
-        })
-    )
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
-});
+router.patch('/:productId', checkAuth, ProductController.updateProducts);
 
 //Delete by id
-router.delete('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    Product.deleteOne({_id: id})
-    .exec()
-    .then(
-        res.status(200).json({
-            message: "Deleted Successfully"
-        })
-    )
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
-});
+router.delete('/:productId', checkAuth, ProductController.deleteProducts);
 
 module.exports = router;
